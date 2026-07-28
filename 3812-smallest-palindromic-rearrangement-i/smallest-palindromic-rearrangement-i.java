4f41419984732d643e3c17537de43382e4954d36
praveennp158-cell <praveenn.p158@gmail.com>
2026-07-28 15:49:15 +0530

    Time: 19 ms (97.19%) | Memory: 48.3 MB (23.69%) - LeetSync

--- 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.java
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.java
@@ -1,14 +1,17 @@
 class Solution {
-
     public String smallestPalindrome(String s) {
-        int len = s.length();
-        int partition = len / 2;
-
+        int n = s.length(), j = 0;
+        int[] freq = new int[26];
         char[] chars = s.toCharArray();
-        Arrays.sort(chars, 0, partition);
 
-        for (int i = 0; i < partition; i++) {
-            chars[len - 1 - i] = chars[i];
+        for (int i = 0; i < n >> 1; i++)
+            freq[(chars[i] & 31) - 1]++;
+
+        for (int i = 0; i < 26; i++) {
+            while (freq[i]-- > 0) {
+                chars[j] = (char) (97 + i);
+                chars[n - 1 - j++] = (char) (97 + i);
+            }
         }
 
         return new String(chars);
