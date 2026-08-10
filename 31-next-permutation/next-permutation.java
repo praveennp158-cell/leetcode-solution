a976c5c795c44fe737d469494e9bfca28f916b42
praveennp158-cell <praveenn.p158@gmail.com>
2026-08-10 14:49:22 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (52.44%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,29 +1,46 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int n = nums.length;
-        int i = n - 2;
 
-        while (i >= 0 && nums[i] >= nums[i + 1]) i--;
+        int pivot = -1;
+        int len = nums.length;
 
-        if (i >= 0) {
-            int j = n - 1;
-            while (nums[j] <= nums[i]) j--;
-
-            int temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
+        // Step 1: Find pivot 🔍
+        for (int i = len - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                pivot = i;
+                break;
+            }
         }
 
-        reverse(nums, i + 1, n - 1);
+        // If no pivot exists, reverse the entire array 🔄
+        if (pivot == -1) {
+            reverse(nums, 0, len - 1);
+            return;
+        }
+
+        // Step 2: Find the rightmost element greater than pivot ➕
+        for (int i = len - 1; i > pivot; i--) {
+            if (nums[pivot] < nums[i]) {
+                swap(nums, pivot, i);
+                break;
+            }
+        }
+
+        // Step 3: Reverse the suffix to get the smallest arrangement ✨
+        reverse(nums, pivot + 1, len - 1);
     }
 
-    private void reverse(int[] nums, int left, int right) {
-        while (left < right) {
-            int temp = nums[left];
-            nums[left] = nums[right];
-            nums[right] = temp;
-            left++;
-            right--;
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
         }
+    }
+
+    private void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
     }
 }
